fde72659d8120fc75060d004b321fa77e17f5b5a
Christian Portillo <cricri_091@hotmail.com>
2018-10-04 17:48:22 -0500

    Add title to histogram.

--- scripts/calculate-words-occurrence.js
+++ scripts/calculate-words-occurrence.js
@@ -14,32 +14,27 @@ const promptConfig = [{
   message: 'Write the path to be analyzed:'
 }];
 
-const getParamQuestions = (questionsConfig) => {
- return questionsConfig.map((config) => ({ ...config }));
-};
-
-async function getPath () {
-  const {path} = await prompts(getParamQuestions(promptConfig), promptOptions);
-
-  if (!!path || path === undefined) return path;
-
-  console.log(`Path (${path}) is invalid`)
-
-  return getPath();
-};
-
-
 getPath()
   .then(function makeCallToAPI (dirPath) {
     if (dirPath === undefined) return;
+
     const uri = `${API_URL}${ENDPOINT}`;
     const qs = { path: dirPath };
 
-    return makeRequest({ uri, qs, json : true });
-  })
-  .then(({data}) => {
-    const histogram = buildHistogram(data, { width: 50, sort: true });
-    console.log(histogram);
+    return makeRequest({ uri, qs, json : true })
+      .then(({data}) => {
+        const histogram = buildHistogram(data, { width: 50, sort: true });
+        
+
+        console.log(`
+            ****************************************************************
+            ***************        Words Occurrence       ******************
+            ****************************************************************
+            PATH = (${dirPath})
+            ****************************************************************
+            \n\n${histogram}
+        `);
+      })
   })
   .then(() => {
     process.exit(0);
@@ -48,3 +43,17 @@ getPath()
     console.log(error.message || error.stack || error);
     process.exit(1);
   });
+  
+  async function getPath () {
+    const {path} = await prompts(getParamQuestions(promptConfig), promptOptions);
+  
+    if (!!path || path === undefined) return path;
+  
+    console.log(`Path (${path}) is invalid`)
+  
+    return getPath();
+  }
+
+  function getParamQuestions(questionsConfig) {
+    return questionsConfig.map((config) => ({ ...config }));
+  }
