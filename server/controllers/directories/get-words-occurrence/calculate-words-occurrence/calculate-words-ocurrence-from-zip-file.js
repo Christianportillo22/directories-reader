6f0a109b59ac665db7e93de0528162ac18dcf0f7
Christian Portillo <cricri_091@hotmail.com>
2018-10-08 11:29:36 -0500

    Improvement Include zip-in-zip word occurrences.

--- server/controllers/directories/get-words-occurrence/calculate-words-occurrence/calculate-words-ocurrence-from-zip-file.js
+++ server/controllers/directories/get-words-occurrence/calculate-words-occurrence/calculate-words-ocurrence-from-zip-file.js
@@ -2,11 +2,20 @@ const ZipFile = require('adm-zip');
 
 const utils = require('./../utils');
 
-function calculateWordOccurrenceFromZipFile(filePath) {
-  const zip = new ZipFile(filePath);
+function calculateWordOccurrenceFromZipFile(filePath, zipFile) {
+  const zip = zipFile ? zipFile : new ZipFile(filePath);
   const files = zip.getEntries();
 
 	return files.reduce((acc, file) => {
+    if (utils.isCompressedFile(file.entryName)) {
+      const nestedZipFile = new ZipFile(file.getCompressedData());
+      const nestedZipFilePath = utils.buildFilePath(filePath, file.entryName);
+
+      const occurrence = calculateWordOccurrenceFromZipFile(nestedZipFilePath, nestedZipFile);
+
+      return utils.accumulate(acc, occurrence);
+    }
+
     if (!utils.isTextFile(file.entryName)) 
       return acc;
     
